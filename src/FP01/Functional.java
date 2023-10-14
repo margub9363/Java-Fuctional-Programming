b09b35caa773578d4874638c4f6da4665e0d05d0
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-10-14 12:45:48 +0530

    Printing Even Number

--- src/FP01/Functional.java
+++ src/FP01/Functional.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 public class Functional {
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12,3,54,532,23,23,45,23));
+        List<Integer> numbers = List.of(12, 3, 54, 532, 23, 23, 45, 23);
+//        printAllNumbersInListFunctional(numbers);
+        printEvenNumber(numbers);
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers){
         numbers.stream().forEach(number -> System.out.println(number));
+    }
+    private static void printEvenNumber(List<Integer> numbers) {
+        numbers.stream().filter(number -> number%2==0).forEach(toPrinnt -> System.out.println(toPrinnt));
     }
 
 }
