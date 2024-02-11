61abb2cafac5071544aed4d1ce04b1cf4b11623f
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-11 16:51:23 +0530

    If we want to Search for individual value with the use of HashMap

--- src/FP01/test/Practice10.java
+++ src/FP01/test/Practice10.java
@@ -1,6 +1,8 @@
 package src.FP01.test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Practice10 {
     public static void main(String[] args) {
@@ -24,6 +26,18 @@ public class Practice10 {
 
 //        I will take the Alphabet Approach to count the idniviual alphabets
 
+
 //        I will take the Map approach to count the idniviual alphabets
+        System.out.println();
+        input="asdfghjQWSDCFVBN oijhgcDFV  B";
+        System.out.println("Starting the Map approach to count individual character");
+        char[] forMapApproach = input.toCharArray();
+        Map<Character,Integer> individualWordCount = new HashMap<Character, Integer>();
+        for(char c: forMapApproach) {
+            if (individualWordCount.containsKey(c)) {
+                individualWordCount.replace(c, individualWordCount.get(c) + 1);
+            } else individualWordCount.put(c, 1);
+        }
+        System.out.println(individualWordCount);
     }
 }
